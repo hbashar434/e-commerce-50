c329d7187f6ce234198547b7f13c08f7f0068699
hbashar434 <h.bashar434@gmail.com>
2023-03-27 03:27:02 +0600

    upto addToCart

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import Product from "../Product/Product";
+import "./Shop.css";
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  console.log(products);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {products.map((product) => (
+          <Product product={product} key={product.id}></Product>
+        ))}
+      </div>
+      <div className="cart-container">{}</div>
+    </div>
+  );
+};
+
+export default Shop;
